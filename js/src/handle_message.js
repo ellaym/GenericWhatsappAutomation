7ba77fadd5ba0fd264b757868c8a0390a4e7a895
Ellay Milstein <ellaym@mail.tau.ac.il>
2024-05-08 20:05:27 +0300

    created seperate main file

--- js/src/handle_message.js
+++
@@ -1,7 +0,0 @@
-// Replace or modify this with your actual message handler logic
-async function handleMessage(message) {
-  console.log("Received message:", message.body);
-  return { response: "Acknowledged: " + message.body };
-}
-
-module.exports = { handleMessage };
